deffd4993a55332f2a412eeece7d7d346d2aeddd
Armand Landry <alandry2834@Conestogac.on.ca>
2025-03-29 02:56:59 -0400

    Added the cart function. I still need to adjust it so it's display drops down from the navbar instead of being locked to the products page. Also, I still don't have access to the backend so I tossed in a dummy item for testing purposes. Feel free to scrub it out once it's compatible with API entries.

--- client-app/src/components/Navbar.js
+++ client-app/src/components/Navbar.js
@@ -1,33 +1,52 @@
-﻿import React from "react";
+﻿import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg"; //path if we need a logo
 import "../styles/Navbar.css";
+import Cart from "./Cart";
 
-function Navbar() {
+function Navbar({ cartItems, removeFromCart }) {
+    const [showCart, setShowCart] = useState(false);
+  
+    const toggleCart = () => {
+      setShowCart((prev) => !prev);
+    };
+  
     return (
-        <nav className="navbar-container">
-            <div className="navbar-logo">
-                {/*<img src={logo} alt="Site Logo" />*/}
-                <span className="navbar-title">VibeStitch</span>
-            </div>
-            <ul className="navbar-links">
-                {/* Links */}
-                {/*<li>*/}
-                {/*    <Link to="/home" className="nav-link">Home</Link>*/}
-                {/*</li>*/}
-                <li>
-                    <Link to="/products" className="nav-link">Products</Link>
-                </li>
-                <li>
-                    <Link to="/about" className="nav-link">About</Link>
-                </li>
-                <li>
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-
-            </ul>
-        </nav>
-    );
+      <nav className="navbar-container">
+        <div className="navbar-logo">
+                {/* <img src={logo} alt="Site Logo" /> */}
+        <span className="navbar-title">VibeStitch</span>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/products" className="nav-link">
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li>
+          {/* Cart Toggle Button */}
+          <button className="nav-link cart-toggle-button" onClick={toggleCart}>
+            Cart ({cartItems.length})
+          </button>
+        </li>
+      </ul>
+      {showCart && (
+        <div className="cart-dropdown">
+          <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
+        </div>
+      )}
+    </nav>
+  );
 }
 
 export default Navbar;
---
+++ client-app/src/test/dummyProducts.js
@@ -0,0 +1,13 @@
+// src/data/dummyProducts.js
+
+const dummyProducts = [
+    {
+      id: 1,
+      name: "Product One",
+      price: 29.99,
+      description: "Description for product one.",
+      image: "/test/TestProduct01.jpg",
+    },
+];
+
+export default dummyProducts;
